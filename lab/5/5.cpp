3293e2748a6ffc6bf68024cb1fa2603e5246babc
manideep1821 <74674951+manideep1821@users.noreply.github.com>
2021-03-05 21:49:15 +0530

    Add files via upload

---
+++ lab/5/5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int stepWays(int array[], int size, int N){
+   int count[N + 1];
+   memset(count, 0, sizeof(count));
+   count[0] = 1;
+   for (int i = 1; i <= N; i++)
+      for (int j = 0; j < size; j++)
+         if (i >= array[j])
+            count[i] += count[i - array[j]];
+   return count[N];
+}
+int main() {
+   int array[] = {1, 2, 3};
+   int size = sizeof(array) / sizeof(array[0]);
+   int N = 10;
+   cout<<"Total number of ways in which the frogs can jump up "<<N<<" stairs is "      <<stepWays(array, size, N);
+   return 0;
+}
