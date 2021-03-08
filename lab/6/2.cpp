134e27002a20b7d147cfd00d420219b9d59c8361
manideep1821 <74674951+manideep1821@users.noreply.github.com>
2021-03-08 09:50:17 +0530

    Update 2.cpp

--- lab/6/2.cpp
+++ lab/6/2.cpp
@@ -2,29 +2,48 @@
 using namespace std;
 int main()
 {
-  string s,k;
-  getline(cin,s);
-  int a=s.length(),flag=0,count=0;
-  for(int i=0;i<a;i++)
-  {
-      if(int(s[i])!=32)
-      {
-         flag++;
-         if(int(s[i+1])==32)
-         {
-             int a=0;
-             for(int j=0;j<flag/2;j++)
-             {
+	string a;
+	cout<<"Enter a string : ";
+	getline(cin,a);
 
-             }
+	int i=0;
+    int j=0,k=0,l=0;
+	cout<<"The palindromes in the given sentence are: ";
+	for(int i=0;i<100;i++)
+	{
 
-         }
-      }
-      else
-      {
-          flag=0;
-      }
-  }
-  return 0;
+		if((a[i]!=' ')&&(a[i]!='\0'))
+		{
+			k++;
+		}
+		else
+		{
+			for(j=1;j<=k/2;j++)
+			{
+				if(a[i-j]!=a[i-1-k+j])
+				{
+					break;
+				}
 
+
+			}
+			if(j>k/2)
+			{
+				l++;
+				for(j=i-k;j!=i;j++)
+				{
+					cout<<a[j];
+				}
+				cout<<" ";
+			}
+			k=0;
+		}
+
+		if(a[i]=='\0')
+		{
+			break;
+		}
+	}
+	cout<<endl<<"The number of paliondromes in the given sentence are  : "<<l<<endl;
+	return 0;
 }
